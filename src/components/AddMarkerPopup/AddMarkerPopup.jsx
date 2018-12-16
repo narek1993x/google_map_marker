19192995c5cd68f38d97dd34557387edf5cf4548
Narek Khachatryan <narek1993x@mail.ru>
2018-12-16 23:11:32 +0400

    change content if relate to isAddMarker

--- src/components/AddMarkerPopup/AddMarkerPopup.jsx
+++ src/components/AddMarkerPopup/AddMarkerPopup.jsx
@@ -23,38 +23,43 @@ export default class AddMarkerPopup extends Component {
   render() {
     const { isAddMarker, markerTitle } = this.state;
     const { onEnter, style } = this.props;
+    let content;
+
+    if (isAddMarker) {
+      content = (
+        <div className='input-wrapper'>
+          <input
+            autoFocus
+            type='text'
+            className='form-control'
+            onFocus={ev => {
+              const currentTarget = ev.currentTarget
+              setTimeout(() => currentTarget.select(), 100)
+            }}
+            onChange={this.onTitleChangeHandler}
+            onKeyDown={e => onEnter(e, markerTitle)}
+          />
+          <span
+            className='cross'
+            onClick={this.hidePopupHanler}>
+            &#x2715;
+          </span>
+        </div>
+      )
+    } else {
+      content = (
+        <div className='button-wrapper'>
+          <p>Add marker here?</p>
+          <div>
+            <button type="button" className="btn btn-info" onClick={this.showInputHandler}>Yes</button>
+            <button type="button" className="btn btn-light" onClick={this.hidePopupHanler}>No</button>
+          </div>
+        </div>
+      )
+    }
     return (
       <div className='addMarker' style={style}>
-        <span className='arrow'></span>
-        {!isAddMarker && (
-          <div className='button-wrapper'>
-            <p>Add marker here?</p>
-            <div>
-              <button type="button" className="btn btn-info" onClick={this.showInputHandler}>Yes</button>
-              <button type="button" className="btn btn-light" onClick={this.hidePopupHanler}>No</button>
-            </div>
-          </div>
-        )}
-        {isAddMarker && (
-          <div className='input-wrapper'>
-            <input
-              type='text'
-              className='form-control'
-              autoFocus
-              onFocus={ev => {
-                const currentTarget = ev.currentTarget
-                setTimeout(() => currentTarget.select(), 100)
-              }}
-              onChange={this.onTitleChangeHandler}
-              onKeyDown={e => onEnter(e, markerTitle)}
-            />
-            <span
-              className='cross'
-              onClick={this.hidePopupHanler}>
-              &#x2715;
-            </span>
-          </div>
-        )}
+        {content}
       </div>
     );
   }
